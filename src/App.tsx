d43c6bc30217c95d58f590b4c075fb0eb2577161
Anton Lebedev <antonlebedev1987av@gmail.com>
2023-11-12 15:04:22 +0300

    add id

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useSearchParams } from 'react-router-dom';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [searchParams] = useSearchParams()
+    const id=searchParams.get("id")
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                <code>Bombagen {id}</code>
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
